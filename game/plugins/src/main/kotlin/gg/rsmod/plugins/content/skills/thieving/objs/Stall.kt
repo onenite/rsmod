a3e2eb0d572078eb0e5b4ba32aac845ec6c48765
Ely <onyx.axe@mail.com>
2021-01-17 07:19:51 -0500

    implemented Thieving stalls and some 193 protocol fixes in packets.yml

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/thieving/objs/Stall.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/thieving/objs/Stall.kt
@@ -8,17 +8,26 @@ import gg.rsmod.plugins.api.cfg.Objs
 import gg.rsmod.plugins.api.ext.*
 import gg.rsmod.plugins.content.skills.thieving.objs.StallRewards.bakers_stall_steals
 import gg.rsmod.plugins.content.skills.thieving.objs.StallRewards.fruit_stall_steals
+import gg.rsmod.plugins.content.skills.thieving.objs.StallRewards.fur_stall_steals
+import gg.rsmod.plugins.content.skills.thieving.objs.StallRewards.seed_stall_steals
 import gg.rsmod.plugins.content.skills.thieving.objs.StallRewards.silk_stall_steals
+import gg.rsmod.plugins.content.skills.thieving.objs.StallRewards.tea_stall_steals
 import gg.rsmod.plugins.content.skills.thieving.objs.StallRewards.vegetable_stall_steals
+import gg.rsmod.plugins.content.skills.thieving.objs.StallRewards.wine_stall_steals
 import kotlin.random.Random
 
 enum class Stall(val stallID: Int,  val minStealLevel: Int, val xpGain: Double, val respawnCycles: Int, val emptyStallId: Int = Objs.MARKET_STALL, val attemptMsg: String = "", val steals: Map<Int, String>) {
     VEGETABLE_STALL(Objs.VEG_STALL, 2, 10.0, 3, steals = vegetable_stall_steals),
     BAKERS_STALL(Objs.BAKERS_STALL, 5, 16.0, 4, steals = bakers_stall_steals),
     BAKERS_STALL2(Objs.BAKERS_STALL_11730, 5, 16.0, 4, steals = bakers_stall_steals),
+    TEA_STALL(Objs.TEA_STALL, 5, 16.0, 8, emptyStallId = Objs.MARKET_STALL, attemptMsg = "a Cup of tea", steals = tea_stall_steals),
+    TEA_STALL2(Objs.TEA_STALL_6574, 5, 16.0, 8, emptyStallId = Objs.MARKET_STALL, attemptMsg = "a Cup of tea", steals = tea_stall_steals),
     SILK_STALL(Objs.SILK_STALL_11729, 20, 24.0, 6, attemptMsg = "some silk", steals = silk_stall_steals),
     SILK_STALL2(Objs.SILK_STALL_36569, 20, 24.0, 6, attemptMsg = "some silk", steals = silk_stall_steals),
+    WINE_STALL(Objs.MARKET_STALL_14011, 22, 27.0, 16, attemptMsg = "some wine", steals = wine_stall_steals),
     FRUIT_STALL(Objs.FRUIT_STALL_28823, 25, 28.5, 3, emptyStallId = Objs.FRUIT_STALL, attemptMsg = "some fruit", steals = fruit_stall_steals),
+    SEED_STALL(Objs.SEED_STALL_7053, 27, 10.0, 16, emptyStallId = Objs.MARKET_STALL, attemptMsg = "some seeds", steals = seed_stall_steals),
+    FUR_STALL(Objs.FUR_STALL_11732, 35, 36.0, 16, emptyStallId = Objs.MARKET_STALL, attemptMsg = "some fur", steals = fur_stall_steals),
 ;
     fun steal(player: Player) {
         if(!player.inventory.hasSpace){
@@ -36,7 +45,11 @@ enum class Stall(val stallID: Int,  val minStealLevel: Int, val xpGain: Double, 
         }
 
         if (attemptMsg != "") {
-            val defaultMsg = "You attempt to steal #STEAL from the the #STALL.".replace("#STEAL", attemptMsg).replace("#STALL", stallID.getObjName(player.world.definitions, lowercase = true))
+            val defaultMsg = when {
+                this == WINE_STALL -> "You attempt to steal #STEAL from the #STALL.".replace("#STEAL", attemptMsg).replace("#STALL", "wine merchant's stall")
+                this == SEED_STALL -> "You attempt to steal #STEAL from the #STALL.".replace("#STEAL", attemptMsg).replace("#STALL", "seed merchant's stall")
+                else -> "You attempt to steal #STEAL from the #STALL.".replace("#STEAL", attemptMsg).replace("#STALL", stallID.getObjName(player.world.definitions, lowercase = true))
+            }
             player.message(defaultMsg)
         }
 
@@ -86,6 +99,31 @@ enum class Stall(val stallID: Int,  val minStealLevel: Int, val xpGain: Double, 
     )
 
      val silk_stall_steals = mapOf(Items.SILK to "some silk")
+
+     val tea_stall_steals = mapOf(Items.CUP_OF_TEA to "a Cup of tea")
+
+     val fur_stall_steals = mapOf(
+         Items.FUR to "some fur",
+         Items.GREY_WOLF_FUR to "some grey wolf fur")
+
+     val wine_stall_steals = mapOf(
+         Items.JUG_OF_WATER to "a Jug of water",
+         Items.JUG_OF_WINE to "a Jug of wine",
+         Items.GRAPES to "some grapes",
+         Items.JUG to "an empty Jug",
+         Items.BOTTLE_OF_WINE to "a bottle of wine")
+
+     val seed_stall_steals = mapOf(
+         Items.POTATO_SEED to "some potato seeds",
+         Items.ONION_SEED to "some onion seeds",
+         Items.CABBAGE_SEED to "some cabbage seeds",
+         Items.TOMATO_SEED to "some tomato seeds",
+         Items.SWEETCORN_SEED to "some sweetcorn seeds",
+         Items.STRAWBERRY_SEED to "some strawberry seeds",
+         Items.WATERMELON_SEED to "some watermelon seeds",
+         Items.SNAPE_GRASS_SEED to "some snape grass seeds")
+
+
 
      val fruit_stall_steals = mapOf(
          Items.COOKING_APPLE to "an apple",
--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/thieving/objs/stalls.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/thieving/objs/stalls.plugin.kts
@@ -1,6 +1,12 @@
 package gg.rsmod.plugins.content.skills.thieving.objs
 
 Stall.values().forEach{ stall ->
+    if (stall == Stall.SEED_STALL){
+        on_obj_option(stall.stallID, "Steal from") {
+            stall.steal(player)
+        }
+        return@forEach
+    }
     on_obj_option(stall.stallID, "Steal-from") {
         stall.steal(player)
     }
